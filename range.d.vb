fdcae5aeae9c02a9691e650177b2bf104b9d8640
Aeraphe <alberto.aeraph@gmail.com>
2021-02-01 00:13:43 -0300

    Add new Definitions for Range Class

--- range.d.vb
+++ range.d.vb
@@ -38,5 +38,120 @@ Public  Sub AddComment(text As String)
 
 End Sub
 
+Public  Sub AddCommentThreaded()
+
+End Sub
+
+Public  Sub AdvancedFilter()
+
+End Sub
+
+Public  Sub AllocateChanges()
+
+End Sub
+
+Public  Sub ApplyName()
+
+End Sub
+
+Public  Sub ApplyOutLineStyles()
+
+End Sub
+
+Public  Sub AutoComplete()
+
+End Sub
+
+Public  Sub AutoFill()
+
+End Sub
+
+Public  Sub AutoFilter()
+
+End Sub
+
+Public  Sub AutioFit()
+
+End Sub
+
+Public  Sub AutoOutline()
+
+End Sub
+
+Public  Sub BorderAround()
+
+End Sub
+
+Public  Sub Calculate()
+
+End Sub
+
+Public  Sub CalculateRowMajorOrder()
+
+End Sub
+
+Public  Sub CheckSpelling()
+
+End Sub
+
+Public  Sub Clear()
+
+End Sub
+
+Public  Sub ClearComments()
+
+End Sub
+
+Public  Sub ClearContents()
+
+End Sub
+
+Public  Sub ClearFormats()
+
+End Sub
+
+Public  Sub ClearHyperlinks()
+
+End Sub
+
+Public  Sub ClearNotes()
+
+End Sub
+
+Public  Sub ClearOutline()
+
+End Sub
+
+Public  Sub ColumnDifferences()
+
+End Sub
+
+Public  Sub Consolidate()
+
+End Sub
+
+Public  Sub ConvertToLinkedDataType()
+
+End Sub
+
+Public  Sub Copy()
+
+End Sub
+
+Public  Sub CopyFromRecordset()
+
+End Sub
+
+Public  Sub CopyPicture()
+
+End Sub
+
+Public  Sub CreateNames()
+
+End Sub
+
+Public  Sub Cut()
+
+End Sub
 
 End Class
